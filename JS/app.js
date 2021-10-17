b785234b86ca2a5f07af82c64b9bbcf782b7a6ec
ortizjoaquin <joaquin.ortizdlc@gmail.com>
2021-10-17 19:16:26 -0300

    added name validation in js

--- JS/app.js
+++ JS/app.js
@@ -1,42 +1,58 @@
-const name = document.getElementById('name')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const age = document.getElementById('age')
-const phone = document.getElementById('phone')
-const adress = document.getElementById('adress')
-const city = document.getElementById('city')
-const postalcode = document.getElementById('postalcode')
-const id = document.getElementById('id')
-const form = document.getElementById('form')
+var lettersAndSpaceOnly = /^[ña-zÑA-Z\s]/
 
-const errorName = document.getElementById('errorname')
+window.onload=function(){
+  // INPUTS VARIABLES
+  var name = document.getElementById('name')
+  var email = document.getElementById('email')
+  var password = document.getElementById('password')
+  var confirmPassword = document.getElementById('confirmPassword')
+  var age = document.getElementById('age')
+  var phone = document.getElementById('phone')
+  var adress = document.getElementById('adress')
+  var city = document.getElementById('city')
+  var postalCode = document.getElementById('postalCode')
+  var id = document.getElementById('id')
+  var form = document.getElementById('form')
+  
+  // ERROR VARIABLES
+  var nameError = document.getElementById('nameError')
+  // var email = document.getElementById('email')
+  // var password = document.getElementById('password')
+  // var age = document.getElementById('age')
+  // var phone = document.getElementById('phone')
+  // var adress = document.getElementById('adress')
+  // var city = document.getElementById('city')
+  // var postalCode = document.getElementById('postalCode')
+  // var id = document.getElementById('id')
+  // var form = document.getElementById('form')
+  
+  
+  // VALIDATING FUNCTIONS
+  // Name validation
+  function checkName(){
+    var spaceBetween = name.value.indexOf(' ')
+    console.log(name.value.length)
+    if (name.value === '' || name.value === null || name.value.length <= 6 || spaceBetween < 0 || spaceBetween === 0 || spaceBetween === name.value.length-1) {
+      nameError.innerText = 'Error, please type full name with more than 6 letters and a space in between';
+      nameError.style.display='block';
+    }
+  }
+  function hideError(){
+    nameError.style.display='none';
+  }
+  name.addEventListener('blur', checkName);
+  name.addEventListener('focus', hideError)
+}
 
-form.addEventListener('submit', (e) => {
-  validarFormulario(e)
-})
+// OUTSIDE WINDOW ONLOAD
 
+//AUTO WRITING NAME
 var getName = ()=>{
   var userName = document.getElementById('name').value;
   document.getElementById('userName').innerHTML=userName;
 }
 
-// validating functions
-
-function validarFormulario(e){
-  let messages=[]
-  const regex = /\d/;
-  console.log(regex.test(e.target.name.value));
-  if (e.target.name.value === '' || e.target.name.value === null) {
-    messages.push('Full name is required')
-  }
-
-  if (e.target.name.value.length <= 6 && e.target.name.value.length > 0) {
-    messages.push('Full name must contain at least 6 letters')
-  }
-
-  if (messages.length > 0) {
-    e.preventDefault()
-    console.log(errorName);
-    errorName.innerText = messages
-  }
+//ON KEY NAME
+function validateKeyName(e){
+  return lettersAndSpaceOnly.test(e.key)
 }
