fda224dfb331233818a17c85c3e1f1e7c99f3682
ortizjoaquin <joaquin.ortizdlc@gmail.com>
2021-10-18 18:55:56 -0300

    finished adding phone, adress, city, postal code and ID validations in js

--- JS/app.js
+++ JS/app.js
@@ -1,35 +1,33 @@
-// OUTSIDE WINDOW ONLOAD
-// AUTO WRITING NAME
+// AUTO WRITING NAME ----------------------------------------------------------------------------------------------
 var getName = ()=>{
   var userName = document.getElementById('name').value;
   document.getElementById('userName').innerHTML=userName;
 }
 
-// RegExp
+// RegExp  ------------------------------------------------------------------------------------------------------------
 var lettersAndSpaceOnly = /^[ña-zÑA-Z\s]/;
-var numbersOnly = /^[\d]/;
+var numbersOnly = /^[0-9]/;
+var lettersNumbersSpacesOnly = /^[ña-zÑA-Z\s0-9]/;
+var mustContainNumbers = /^(?=.*[0-9])/;
 var mustContainLettersAndNumbers = /^(?=.*[0-9])(?=.*[a-zA-Z])/;
+var mustContainLettersNumbersSpaces = /^(?=.*[0-9])(?=.*[a-zA-Z\s])/;
+var containSpaces = /\s/;
 
-// ON KEY NAME
+// OnKey for the name input (only allows you to enter letters and spaces at the name input) ---------------------------
 function validateKeyName(e){
   return lettersAndSpaceOnly.test(e.key)
 }
 
-// ON KEY AGE
-function validateKeyAge(e){
-  return numbersOnly.test(e.key)
-}
-
-// HIDE ERROR FUNCTION
+// HIDE ERROR FUNCTION (this makes the error messages to be non-displayed as default) ---------------------------------
 function hideError(errorMessage){
   console.log(errorMessage)
   errorMessage.style.display='none';
 }
 
+// DECLARED FUNCTIONS FOR VALIDATING (first declared, later on called to work) ----------------------------------------
 
-// DECLARED FUNCTIONS FOR VALIDATING
-// remember, in this case nameValue = name.value
 // Name validation
+// remember, in this case nameValue = name.value
 function checkName(nameValue){
   var spaceBetween = nameValue.indexOf(' ');
   if (nameValue === '' || nameValue === null || nameValue.length <= 6 || spaceBetween < 0 || nameValue.startsWith(' ') || nameValue.endsWith(' ')) {
@@ -38,38 +36,44 @@ function checkName(nameValue){
   return true
 }
 
-// remember, in this case emailValue = email.value
 // Email validation
+// remember, in this case emailValue = email.value
 function checkEmail(emailValue){
-    var atSymbol = emailValue.indexOf("@");
-    if(atSymbol < 1){
-      return false;
-    }
-    var domain = emailValue.slice(atSymbol);
-    var dot = domain.indexOf(".");
-    if(dot < 0 || dot <= 2){
-      return false;
-    }
-    if(emailValue.endsWith('.') || emailValue.startsWith('.')){
-      return false;
-    }
-    return true;
-  }
-
-// remember, in this case passwordValue = password.value
-// Password validation
-function checkPassword(passwordValue){
-  if(!mustContainLettersAndNumbers.test(passwordValue)){
+  var atSymbol = emailValue.indexOf("@");
+  if(atSymbol < 1){
     return false;
   }
-  if (passwordValue === '' || passwordValue === null || passwordValue.length <= 8) {
+  var domain = emailValue.slice(atSymbol);
+  var dot = domain.indexOf(".");
+  if(dot < 0 || dot <= 2){
+    return false;
+  }
+  if(emailValue.endsWith('.') || emailValue.startsWith('.')){
+    return false;
+  }
+  if(containSpaces.test(emailValue)){
     return false;
   }
   return true;
 }
 
-// remember, in this case confirmPasswordValue = confirmPassword.value
+// Password validation
+// remember, in this case passwordValue = password.value
+function checkPassword(passwordValue){
+  if(!mustContainLettersAndNumbers.test(passwordValue)){
+    return false;
+  }
+  if(containSpaces.test(passwordValue)){
+    return false;
+  }
+  if (passwordValue === '' || passwordValue === null || passwordValue.length < 7) {
+    return false;
+  }
+  return true;
+}
+
 // Confirm password validation
+// remember, in this case confirmPasswordValue = confirmPassword.value
 function checkConfirmPassword(confirmPasswordValue){
   if (confirmPasswordValue === password.value) {
     return true;
@@ -79,42 +83,108 @@ function checkConfirmPassword(confirmPasswordValue){
   }
 }
 
-// remember, in this case ageValue = age.value
 // Age validation
+// remember, in this case ageValue = age.value
 function checkAge(ageValue){
   if (ageValue === '' || ageValue === null || ageValue < 18 || ageValue % 1 != 0) {
+    return false;
+  }
+  if (!numbersOnly.test(ageValue)) {
     return false;
   }
   return true;
 }
 
+// Phone validation
+// remember, in this case phoneValue = phone.value
+function checkPhone(phoneValue){
+  if (phoneValue === '' || phoneValue === null || phoneValue.length < 7) {
+    return false;
+  }
+  if (!numbersOnly.test(phoneValue)) {
+    return false;
+  }
+  return true;
+}
+
+// Adress validation
+// remember, in this case adressValue = adress.value
+function checkAdress(adressValue){
+  var spaceBetween = adressValue.indexOf(' ');
+  if (adressValue === '' || adressValue === null || adressValue.length < 5 || spaceBetween < 0 || adressValue.startsWith(' ') || adressValue.endsWith(' ')) {
+    return false
+  }
+  if(!mustContainLettersNumbersSpaces.test(adressValue)){
+    return false;
+  }
+  return true
+}
+
+// City validation
+// remember, in this case cityValue = city.value
+function checkCity(cityValue){
+  if (cityValue === '' || cityValue === null || cityValue.length < 3 || cityValue.startsWith(' ') || cityValue.endsWith(' ')) {
+    return false
+  }
+  return true
+}
+
+// Postal Code validation
+// remember, in this case postalCodeValue = postalCode.value
+function checkPostalCode(postalCodeValue){
+  if (postalCodeValue === '' || postalCodeValue === null || postalCodeValue.length < 3 || postalCodeValue.startsWith(' ') || postalCodeValue.endsWith(' ')) {
+    return false
+  }
+  if(!lettersNumbersSpacesOnly.test(postalCodeValue)){
+    return false;
+  }
+  if(!mustContainNumbers.test(postalCodeValue)){
+    return false;
+  }
+  return true
+}
+
+// ID validation
+// remember, in this case idValue = id.value
+function checkId(idValue){
+  if (idValue === '' || idValue === null || idValue.length < 7 || idValue.length > 8) {
+    return false
+  }
+  return true
+}
+
+// WINDOWS ONLOAD (fixes getElement issues with empty inputs) ---------------------------------------------------------
+
 window.onload=function(){
-  // INPUTS VARIABLES
+
+  // INPUTS VARIABLES -------------------------------------------------------------------------------------------------
   var name = document.getElementById('name')
   var email = document.getElementById('email')
   var password = document.getElementById('password')
   var confirmPassword = document.getElementById('confirmPassword')
   var age = document.getElementById('age')
   var phone = document.getElementById('phone')
-  // var adress = document.getElementById('adress')
-  // var city = document.getElementById('city')
-  // var postalCode = document.getElementById('postalCode')
-  // var id = document.getElementById('id')
+  var adress = document.getElementById('adress')
+  var city = document.getElementById('city')
+  var postalCode = document.getElementById('postalCode')
+  var id = document.getElementById('id')
   // var form = document.getElementById('form')
   
-  // ERROR VARIABLES
+  // ERROR VARIABLES --------------------------------------------------------------------------------------------------
   var nameError = document.getElementById('nameError')
   var emailError = document.getElementById('emailError')
   var passwordError = document.getElementById('passwordError')
   var confirmPasswordError = document.getElementById('confirmPasswordError')
   var ageError = document.getElementById('ageError')
   var phoneError = document.getElementById('phoneError')
-  // var adress = document.getElementById('adress')
-  // var city = document.getElementById('city')
-  // var postalCode = document.getElementById('postalCode')
-  // var id = document.getElementById('id')
-  // var form = document.getElementById('form')
+  var adressError = document.getElementById('adressError')
+  var cityError = document.getElementById('cityError')
+  var postalCodeError = document.getElementById('postalCodeError')
+  var idError = document.getElementById('idError')
+  // var formError = document.getElementById('formError')
   
+  //-------------------------------------------------------------------------------------------------------------------
+
   // VALIDATING FUNCTIONS
   // Name validation (blur and focus)
   name.addEventListener('blur', function(){
@@ -137,7 +207,7 @@ window.onload=function(){
   // Password validation (blur and focus)
   password.addEventListener('blur', function(){
     if (checkPassword(password.value)===false){
-      passwordError.innerText = 'Please type a valid password';
+      passwordError.innerText = 'Please type a valid password (must contain at least 8 characters, mixing numbers and letters only)';
       passwordError.style.display='block';
     }
   });
@@ -155,10 +225,55 @@ window.onload=function(){
   // Age validation (blur and focus)
   age.addEventListener('blur', function(){
     if (checkAge(age.value)===false){
-      ageError.innerText = 'Must be at least 18 years old';
+      ageError.innerText = 'Please enter a valid age (numbers only), you must be at least 18 years old';
       ageError.style.display='block';
     }
   });
   age.addEventListener('focus', function(){hideError(ageError)})
+
+  // Phone validation (blur and focus)
+  phone.addEventListener('blur', function(){
+    if (checkPhone(phone.value)===false){
+      phoneError.innerText = 'Please enter a valid phone number (numbers only)';
+      phoneError.style.display='block';
+    }
+  });
+  phone.addEventListener('focus', function(){hideError(phoneError)})
+
+  // Adress validation (blur and focus)
+  adress.addEventListener('blur', function(){
+    if (checkAdress(adress.value)===false){
+      adressError.innerText = 'Please enter a valid adress (street name and number separated by a space)';
+      adressError.style.display='block';
+    }
+  });
+  adress.addEventListener('focus', function(){hideError(adressError)})
+
+  // City validation (blur and focus)
+  city.addEventListener('blur', function(){
+    if (checkCity(city.value)===false){
+      cityError.innerText = 'Please enter a valid city name';
+      cityError.style.display='block';
+    }
+  });
+  city.addEventListener('focus', function(){hideError(cityError)})
+
+  // Postal Code validation (blur and focus)
+  postalCode.addEventListener('blur', function(){
+    if (checkPostalCode(postalCode.value)===false){
+      postalCodeError.innerText = 'Please enter a valid Postal Code (numeric or alphanumeric)';
+      postalCodeError.style.display='block';
+    }
+  });
+  postalCode.addEventListener('focus', function(){hideError(postalCodeError)})
+
+  // ID validation (blur and focus)
+  id.addEventListener('blur', function(){
+    if (checkId(id.value)===false){
+      idError.innerText = 'Please enter a valid ID number (7 or 8 digits)';
+      idError.style.display='block';
+    }
+  });
+  id.addEventListener('focus', function(){hideError(idError)})
 
 }
